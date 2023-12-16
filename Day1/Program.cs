af742acf4d44ca828391119006fe16d1dac37154
Aleksandr Schemelev <aleksandr.schemelev>
2023-12-16 13:17:35 +0300

    feat: add Day1 solution

---
+++ Day1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+var projectDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName;
+var filePath = Path.Combine(projectDirectory!, "input.txt");
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine("File does not exist.");
+    return;
+}
+
+using var reader = new StreamReader(filePath);
+var sum = 0;
+
+while (reader.ReadLine() is { } line)
+{
+    string numbers = Regex.Replace(line, @"[^\d]", "");
+    int firstNumber = int.Parse(numbers[..1]);
+    int lastNumber = int.Parse(numbers[^1..]);
+
+    int number = firstNumber * 10 + lastNumber;
+    sum += number;
+}
+
+Console.WriteLine(sum);
